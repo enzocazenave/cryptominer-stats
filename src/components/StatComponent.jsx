4b45061483af9508b0471ede33105c04dddeceb9
enzocazenave <encaze24@hotmail.com>
2022-09-05 14:23:38 -0300

    Add usd value of payout

--- src/components/StatComponent.jsx
+++ src/components/StatComponent.jsx
@@ -1,7 +1,9 @@
 import { convertData, translateStat, convertToUsd } from '../helpers';
 
 export const StatComponent = ({stat, statValue, price}) => {
+
     const stat_const = stat;
+    
     statValue = convertData(stat, statValue);
     stat = translateStat(stat);
 
@@ -9,7 +11,7 @@ export const StatComponent = ({stat, statValue, price}) => {
         <div className="stat-component">
             <span className="stat-component__value">{ statValue }</span>
             {
-                (stat_const == 'balance' || stat_const == 'paid' || stat_const == 'reward24') &&
+                (stat_const === 'balance' || stat_const === 'paid' || stat_const === 'reward24' || stat_const === 'payout') &&
                     <span className="stat-component__convertion">{ convertToUsd(statValue, price) } USD</span>
             }
             <span className="stat-component__title">{ stat }</span>
